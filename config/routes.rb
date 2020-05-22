66bcf54f545ccd82353a2dec66104fe3389533d9
Craig Christopher Messi <craig.messi@gmail.com>
2020-05-22 03:31:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :doctors
-  resources :appointments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :patients
   resources :appointments, only: [:show]
